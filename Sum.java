48b07f5377ffd855b41da5eb055e2d5e1456c789
Yugdarshan Shinde <yugdarshan11@gmail.com>
2022-08-09 18:49:56 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum{
+    public static void main(String[] args)
+    {
+        int a = 10 , b =20;
+
+        System.out.println("Sum of A + B: " + a+b);
+    }
+}
---
+++ divide.java
@@ -0,0 +1,8 @@
+public class divide{
+    public static void main(String[] args)
+    {
+        int a = 10, b = 20;
+
+        System.out.println("Division of A * B :" + a*b);
+    }
+}
